de2e81cb2cee9ffaced7ac69934933e27a711212
Lars Stoltenow <penma@penma.de>
2009-05-02 21:45:29 +0200

    removed clang hack, bug has been fixed (go update)

--- dizzy_global.h
+++ dizzy_global.h
@@ -3,15 +3,9 @@
 
 #define _XOPEN_SOURCE 500
 
-#ifdef __clang__
-	#define __NO_INLINE__ 1
-#endif
 #include <math.h>
 #include <stdlib.h>
 #include <stdint.h>
-#ifdef __clang__
-	#undef __NO_INLINE__
-#endif
 
 #endif
 
