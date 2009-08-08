837bf988132e614844df8a0ced4b143c41305aaa
Lars Stoltenow <penma@ixion.penma.de>
2009-08-08 18:34:01 +0200

    textures.c: code refactor; split dizzytextures_generate_textures

--- textures.c
+++ textures.c
@@ -23,41 +23,48 @@ void dizzytextures_set_resolution(struct dizzytextures *dt, int res) {
 	dt->resolution = res;
 }
 
-void dizzytextures_generate_textures(struct dizzytextures *dt) {
+GLuint dizzytextures_generate_texture(struct dizzytextures *dt, double (*texture_func)(double, double)) {
 	unsigned char *texture;
 	texture = malloc(dt->resolution * dt->resolution * (24 / 8));
 
 	unsigned int texel;
 	GLuint texid;
 
-	for (int textype = 0; textype < dizzytextures_data_count; textype++) {
-		for (int x = 0; x < dt->resolution; x++) {
-			for (int y = 0; y < dt->resolution; y++) {
-				double nx = ((double)x / dt->resolution) - 0.5;
-				double ny = ((double)y / dt->resolution) - 0.5;
+	for (int x = 0; x < dt->resolution; x++) {
+		for (int y = 0; y < dt->resolution; y++) {
+			double nx = ((double)x / dt->resolution) - 0.5;
+			double ny = ((double)y / dt->resolution) - 0.5;
 
-				double txval = dizzytextures_data_funcs[textype](nx, ny);
-				if (txval > 1.0)
-					txval = 1.0;
-				if (txval < 0.0)
-					txval = 0.0;
-				texel = (unsigned char)(txval * 255);
+			double txval = texture_func(nx, ny);
+			if (txval > 1.0)
+				txval = 1.0;
+			if (txval < 0.0)
+				txval = 0.0;
+			texel = (unsigned char)(txval * 255);
 
-				texture[(x * dt->resolution + y) * 3    ] = texel;
-				texture[(x * dt->resolution + y) * 3 + 1] = texel;
-				texture[(x * dt->resolution + y) * 3 + 2] = texel;
-			}
+			texture[(x * dt->resolution + y) * 3    ] = texel;
+			texture[(x * dt->resolution + y) * 3 + 1] = texel;
+			texture[(x * dt->resolution + y) * 3 + 2] = texel;
 		}
+	}
 
-		/* generate the texture */
-		glGenTextures(1, &texid);
-		glBindTexture(GL_TEXTURE_2D, texid);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexImage2D(GL_TEXTURE_2D, 0, 3, dt->resolution, dt->resolution, 0, GL_RGB, GL_UNSIGNED_BYTE, texture);
-		
+	/* load the texture into opengl */
+	glGenTextures(1, &texid);
+	glBindTexture(GL_TEXTURE_2D, texid);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, dt->resolution, dt->resolution, 0, GL_RGB, GL_UNSIGNED_BYTE, texture);
+
+	return texid;
+}
+
+
+
+
+void dizzytextures_generate_textures(struct dizzytextures *dt) {
+	for (int textype = 0; textype < dizzytextures_data_count; textype++) {
 		dt->textures = realloc(dt->textures, sizeof(GLuint) * (dt->textures_count + 1));
-		dt->textures[dt->textures_count] = texid;
+		dt->textures[dt->textures_count] = dizzytextures_generate_texture(dt, dizzytextures_data_funcs[textype]);
 		dt->textures_count++;
 	}
 }
