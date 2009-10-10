411406dc754e2e4d8228dc7611865ca3de97f10e
Lars Stoltenow <penma@penma.de>
2009-10-10 23:12:17 +0200

    Textures/Default: remove perl versions of textures
    They're not needed anymore, as GLSL2Perl is now up and running.
    
    S-expr versions of the not yet converted subs are provided in source comments.

--- lib/Dizzy/Textures/Default.pm
+++ lib/Dizzy/Textures/Default.pm
@@ -22,11 +22,6 @@ my $sf_wrapval = << "// END FUNCTION";
 my @textures = (
 	{
 		name => "Ornament",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return sin(pi / (0.0001 + 2 * $dist)) / 2 + 0.5;
-		},
 		shader => << "		// END SHADER",
 			void main() {
 				float dist = length(gl_TexCoord[0].xy - 0.5);
@@ -37,50 +32,26 @@ my @textures = (
 	},
 	{
 		name => "Spots",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return cos($dist * pi) / 2 + 0.5;
-		},
+		# (t2l (cos (* DIST pi)))
 	},
 	{
 		name => "Aurora",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return cos($dist * pi + $y / ($dist + 0.0001)) / 2 + 0.5;
-		},
+		# (t2l (cos (+ (* DIST pi) (/ Y DIST))))
 	},
 	{
 		name => "Flowers",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return cos($dist * pi + sin(asin($y / ($dist + 0.00001)) * 8) * 0.2) / 2 + 0.5;
-		},
+		# (t2l (cos (+ (* DIST pi) (* 0.2 (sin (* 8 (asin (/ Y DIST))))))))
 	},
 	{
 		name => "Blurred Circles",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return sin($dist * pi / 2) / 2 + 0.5;
-		},
+		# (t2l (sin (* DIST pi 0.5)))
 	},
 	{
 		name => "Blurred Anticircles",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return cos($dist * pi + sin(asin($y / ($dist + 0.00001)) * 2) * 0.2) / 2 + 0.5;
-		},
+		# return cos($dist * pi + sin(asin($y / ($dist + 0.00001)) * 2) * 0.2) / 2 + 0.5;
 	},
 	{
 		name => "Waves",
-		function => sub {
-			my ($x, $y) = @_;
-			return wrapval((cos($y * pi) + sin($x * pi)) / 2 + 0.5);
-		},
 		shader => $sf_wrapval . << "		// END SHADER",
 			void main() {
 				float val = wrapval((cos(gl_TexCoord[0].y * 3.141) + sin(gl_TexCoord[0].x * 3.141)) / 2. + 0.5);
@@ -90,11 +61,6 @@ my @textures = (
 	},
 	{
 		name => "Crystal",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return cos(($x * 2) + asin($y / ($dist + 0.0001))) / 2 + 0.5;
-		},
 		shader => << "		// END SHADER",
 			void main() {
 				float dist = length(gl_TexCoord[0].xy - 0.5);
@@ -108,11 +74,6 @@ my @textures = (
 	},
 	{
 		name => "Black Circles",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return 1.0 - (cos($dist * pi) / 2 + 0.5);
-		},
 		shader => << "		// END SHADER",
 			void main() {
 				float dist = length(gl_TexCoord[0].xy - 0.5);
@@ -123,11 +84,6 @@ my @textures = (
 	},
 	{
 		name => "Hexagon",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return wrapval(asin($y / ($dist + 0.0001)) / 2 + 0.5);
-		},
 		shader => $sf_wrapval . << "		// END SHADER",
 			void main() {
 				float dist = length(gl_TexCoord[0].xy - 0.5);
@@ -138,10 +94,6 @@ my @textures = (
 	},
 	{
 		name => "Boxes",
-		function => sub {
-			my ($x, $y) = @_;
-			return ($x + 0.5) * ($y + 0.5);
-		},
 		shader => << "		// END SHADER",
 			void main() {
 				float val = gl_TexCoord[0].x * gl_TexCoord[0].y;
@@ -151,35 +103,18 @@ my @textures = (
 	},
 	{
 		name => "Bubbles",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return cosec($dist + 0.1) / 3;
-		},
+		# (/ (cosec (+ DIST 0.1)) 3)
 	},
 	{
 		name => "Winter Dream",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return cosec($dist * 3 + 0.1) / 3;
-		},
+		# (/ (cosec (+ (* DIST 3) 0.1)) 3)
 	},
 	{
 		name => "Pills",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return -log($dist + 0.01);
-		},
+		# (- 0 (ln (+ dist 0.01)))
 	},
 	{
 		name => "Stars",
-		function => sub {
-			my ($x, $y) = @_;
-			my $dist = sqrt($x ** 2 + $y ** 2);
-			return 22.35468769 * $dist**6 + sin(12) * $dist**2 / 5.734;
-		},
 		shader => << "		// END SHADER",
 			void main() {
 				float dist = length(gl_TexCoord[0].xy - 0.5);
